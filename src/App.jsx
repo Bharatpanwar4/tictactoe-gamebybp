f7595d3f8bf817001ccfc3e8e1b5f1105a4d2be7
Bharatpanwar4 <111150558+Bharatpanwar4@users.noreply.github.com>
2023-05-07 19:08:53 +0530

    used hook use state'

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className='app'>
-<Board/>
+  <Board/>
     </div>
     
   )
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,29 +1,52 @@
+import { useState } from 'react';
+import Square from './Square';
+const Board = () => {
 
-import Square from "./Square";
-const Board =()=>{
-    return <div className="board">
-    <div className="board-row">
-        <Square value={0}/>
-        <Square value={1}/>
-        <Square value={2}/>
+const [squares ,setSquares] = useState(Array(9).fill(null))
+console.log(squares);
+const handleSquareClick = (clickedPosition)=>{
 
-    </div>
-    <div className="board-row">
-    <Square value={3}/>
-        <Square value={4}/>
-        <Square value={5}/>
+setSquares((currentSquare)=>{
+return currentSquare.map((squareValue,position)=>{
+  if(clickedPosition === position){
+    // console.log(squareValue);
+    return 'X'
 
-    </div>
-    <div className="board-row">
-    <Square value={6}/>
-        <Square value={7}/>
-        <Square value={8}/>
+  }
+  return squareValue
+})
+})
 
-    </div>
 
-    
-    
-    </div>
 }
+
+const renderSquare = (position)=>{
+    return(
+        <Square value={squares[position]} onClick={()=>handleSquareClick(position)} />
+    )
+}
+
+  return (
+    <div className="board">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+
+        
+      </div>
+      <div className="board-row">
+      {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+      {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
 
 export default Board;
